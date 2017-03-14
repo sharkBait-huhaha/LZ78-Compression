4adb8807a1b3f1570983350185e521945c9716a9
Joseph Boyd <josephboyd9@gmail.com>
2017-03-15 11:25:42 +1300

    Bug fixed. Now reads from std input and writes correctly encoded file to std output.

--- encoder.java
+++ encoder.java
@@ -6,63 +6,31 @@ import java.util.ArrayList;
 
 class Encoder{
     public static void main(String[] args){
-	//Note to self: make nextID a static variable of TrieNode
-	int bufMax=256, nextID=1;
-	File fileOut;
-	FileInputStream in;
-	FileOutputStream out;
+	int bufMax=256;
 	TrieNode trie=new TrieNode(0,(byte)0);
-	byte c, buf[]=new byte[bufMax], temp;
+	int c, buf[]=new int[bufMax], temp;
 	String s, pair="", next="";
 	
         try{
 	    /*if(args.length!=2)
-	    	throw new IOException();
-            in=new FileInputStream(args[0]);
-	    fileOut=new File(args[1]);
-	    if(!fileOut.createNewFile()){
-		System.out.println("Doing this will override file: "+args[1]);
-		System.out.println("Are you sure you wish to continue?(y/n)");
-		boolean cont=false;
-		while(!cont){
-		    s=System.console().readLine();
-		    if(s.equals("y"))
-			cont=true;
-		    else if(s.equals("n"))
-			System.exit(0);
-		    else
-			System.out.println("please enter 'y' for yes, or 'n' for no. ('n' will close the program) ");
-		}
-		fileOut.delete();
-		fileOut.createNewFile();
-	    }
-	    out=new FileOutputStream(args[1]);
-	    */
-            while((c=(byte)System.in.read())!=(byte)-1){
+	      throw new IOException();*/
+            while((c=System.in.read())!=-1){
+		//System.out.println(c);
 		for(int i=0;i<buf.length;i++)
-		    if(buf[i]==(byte)0){
+		    if(buf[i]==0){
 			buf[i]=c;
 			i=buf.length;
 		    }
-		if((next=trie.find(buf)).length()==2){
-		    pair=next;
-		}else{
+		if((pair=trie.find(buf)).length()==2){
+		    //pair=next;
+		    //}else{
 		    System.out.println(pair);
-		    //is there a way to add the child without a second traverse of the trie?
-		    trie.add(nextID, buf);
-		    nextID++;
-		    temp=buf[buf.length-1];
-		    buf=new byte[bufMax];
-		    buf[0]=temp;
+		    buf=new int[bufMax];
+		    //buf[0]=c;
 		}
             }
-	    if(buf[0]!=(byte)0)
-		System.out.print(pair);
-	    System.out.print("\u0004");
-	    
-	    //}catch(FileNotFoundException e){
-	    //System.out.println("Invalid file given as argument");
-	    //System.out.println("Usage: java Encoder <File Input> <File Output>");
+	    if(buf[0]!=0)
+		System.out.println(pair);
 	}catch(IOException e){
 	    System.err.println("Usage: java Encoder <File Input> <File Output>");
 	}catch(Exception e){
@@ -74,16 +42,17 @@ class Encoder{
 
 class TrieNode{
     final int ID;
-    final byte DATA;
+    final int DATA;
+    static int nextID = 1;
     private ArrayList<TrieNode> children=new ArrayList<TrieNode>();
     
-    TrieNode(int id,byte data){
+    TrieNode(int id,int data){
 	ID=id;
 	DATA=data;
     }
-    void add(int id,byte[] data){
-	if(data[1]!=(byte)0){
-	    byte[] tail=new byte[data.length-1];
+    void add(int id,int[] data){
+	if(data[1]!=0){
+	    int[] tail=new int[data.length-1];
 	    System.arraycopy(data,1,tail,0,tail.length);
 	    for(TrieNode t:children)
 		if(t.DATA==data[0])
@@ -91,17 +60,20 @@ class TrieNode{
 	}else
 	    children.add(new TrieNode(id,data[0]));
     }
-    String find(byte[] data){
+    String find(int[] data){
 	for(TrieNode t:children)
 	    if(t.DATA==data[0]){
-		if(data[1]!=(byte)0){
-		    byte[] tail=new byte[data.length-1];
+		if(data[1]!=0){
+		    int[] tail=new int[data.length-1];
 		    System.arraycopy(data,1,tail,0,tail.length);
 		    //System.out.println(tail.toString());
 		    return t.find(tail);
 		}else
 		    return Integer.toString(t.ID);
 	    }
-	return Integer.toString(ID)+(char)data[0];
+	TrieNode t = new TrieNode(nextID, data[0]);
+	nextID++;
+	children.add(t);
+	return Integer.toString(ID) + (char)data[0];
     }
 }
