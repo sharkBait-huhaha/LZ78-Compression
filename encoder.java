ac033efef4658c6332facde8c22e48b11b6e0ef0
Joseph Boyd <josephboyd9@gmail.com>
2017-03-15 12:38:07 +1300

    implemeted reset function, printing '\u001B' (Unicode escape character)

--- encoder.java
+++ encoder.java
@@ -7,13 +7,15 @@ import java.util.ArrayList;
 class Encoder{
     public static void main(String[] args){
 	int bufMax=256;
-	TrieNode trie=new TrieNode(0,(byte)0);
-	int c, buf[]=new int[bufMax], temp;
-	String s, pair="", next="";
+	int maxSize;
+	TrieNode trie=new TrieNode(0);
+	int c, buf[]=new int[bufMax];
+	String[] pair = {""};
 	
         try{
-	    /*if(args.length!=2)
-	      throw new IOException();*/
+	    if(args.length!=1)
+	      throw new IOException();
+	    maxSize=Integer.parseInt(args[0]);
             while((c=System.in.read())!=-1){
 		//System.out.println(c);
 		for(int i=0;i<buf.length;i++)
@@ -21,45 +23,44 @@ class Encoder{
 			buf[i]=c;
 			i=buf.length;
 		    }
-		if((pair=trie.find(buf)).length()==2){
-		    //pair=next;
-		    //}else{
-		    System.out.println(pair);
+		if((pair=trie.find(buf).split(" ")).length==2){
+		    System.out.print(pair[0]);
+		    System.out.println(pair[1]);
+		    if(dropZeros(Integer.toBinaryString(trie.nextID)).length()>maxSize){
+			System.out.println("\u001B");
+			trie.reset();
+		    }
 		    buf=new int[bufMax];
-		    //buf[0]=c;
 		}
             }
 	    if(buf[0]!=0)
 		System.out.println(pair);
 	}catch(IOException e){
-	    System.err.println("Usage: java Encoder <File Input> <File Output>");
+	    System.err.println("Usage: java Encoder <Dictionary Size(bits)>");
 	}catch(Exception e){
 	    System.err.println("error:");
 	    e.printStackTrace();
 	}
+    }
+    private static String dropZeros(String str){
+	while(str.charAt(0)=='0')
+	    str=str.substring(1);
+	return str;
     }
 }
 
 class TrieNode{
     final int ID;
     final int DATA;
-    static int nextID = 1;
+    static int nextID = 0;
     private ArrayList<TrieNode> children=new ArrayList<TrieNode>();
     
-    TrieNode(int id,int data){
-	ID=id;
+    TrieNode(int data){
+	ID=nextID;
 	DATA=data;
+	nextID++;
     }
-    void add(int id,int[] data){
-	if(data[1]!=0){
-	    int[] tail=new int[data.length-1];
-	    System.arraycopy(data,1,tail,0,tail.length);
-	    for(TrieNode t:children)
-		if(t.DATA==data[0])
-		    t.add(id,data);
-	}else
-	    children.add(new TrieNode(id,data[0]));
-    }
+
     String find(int[] data){
 	for(TrieNode t:children)
 	    if(t.DATA==data[0]){
@@ -71,9 +72,12 @@ class TrieNode{
 		}else
 		    return Integer.toString(t.ID);
 	    }
-	TrieNode t = new TrieNode(nextID, data[0]);
-	nextID++;
+	TrieNode t = new TrieNode(data[0]);
 	children.add(t);
-	return Integer.toString(ID) + (char)data[0];
+	return Integer.toString(ID) +" "+ (char)data[0];
+    }
+    void reset(){
+	children=new ArrayList<TrieNode>();
+	nextID=1;
     }
 }
