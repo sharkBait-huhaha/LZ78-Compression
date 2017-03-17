dacade15942b89210207a07619bedd773b3ad1e9
Joseph Boyd <josephboyd9@gmail.com>
2017-03-17 16:08:55 +1300

    fixed  bugs: spaces not going to output and random characters at the end of output.

--- encoder.java
+++ encoder.java
@@ -10,20 +10,19 @@ class Encoder{
 	int maxSize;
 	TrieNode trie=new TrieNode(0);
 	int c, buf[]=new int[bufMax];
-	String[] pair = {""};
+	String[] pair = {null};
 	
         try{
 	    if(args.length!=1)
 	      throw new IOException();
 	    maxSize=Integer.parseInt(args[0]);
             while((c=System.in.read())!=-1){
-		//System.out.println(c);
 		for(int i=0;i<buf.length;i++)
 		    if(buf[i]==0){
 			buf[i]=c;
 			i=buf.length;
 		    }
-		if((pair=trie.find(buf).split(" ")).length==2){
+		if((pair=trie.find(buf).split(" ", 2)).length==2){
 		    System.out.print(pair[0]);
 		    System.out.println(pair[1]);
 		    if(dropZeros(Integer.toBinaryString(trie.nextID)).length()>maxSize){
@@ -31,10 +30,11 @@ class Encoder{
 			trie.reset();
 		    }
 		    buf=new int[bufMax];
+		    pair=new String[0];
 		}
             }
-	    if(buf[0]!=0)
-		System.out.println(pair);
+	    for(String str: pair)
+	    	System.out.println(str);
 	}catch(IOException e){
 	    System.err.println("Usage: java Encoder <Dictionary Size(bits)>");
 	}catch(Exception e){
