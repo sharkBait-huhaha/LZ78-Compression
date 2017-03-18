3fff1b03a77e7e920c2a73620ee5c5fa99586d14
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2017-03-18 18:56:00 +1300

    Using List instead of arrays to store data

--- src/decoder.java
+++ src/decoder.java
@@ -4,16 +4,17 @@
 
 import java.io.*;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 class decoder
 {
     protected static int index =1;
-    protected static int[] IDArray = new int[20];
-    protected static int[] DataArray = new int[20];
 
-
+    protected static List<Integer> Data = new ArrayList<Integer>();
+    
     public static void main(String []args)
     {
         System.err.println();
@@ -23,41 +24,36 @@ class decoder
         //Get Standard Input
         try
         {
-            IDArray[0] = 0;
-            DataArray[0] = 0;
+            Data.add(0);
             System.err.println("");
             System.err.println("--  INPUT  --");
             //Store Nothing in 0 index of arrays
             while((input =System.in.read()) != -1)
             {
-
                 int inputInINT = getIntFromAxcii(input);
+                if (inputInINT != 100001) {
+                    if (Data.get(inputInINT) == 0) {
 
-                    if (inputInINT != 100001) {
-                        if (DataArray[inputInINT] == 0) {
-                            IDArray[index] = inputInINT;
+                        input = System.in.read();
+                        Data.add(input);
 
-                            input = System.in.read();
-                            DataArray[index] = input;
+                        System.out.print(getIntToString(Data.get(index)));
 
-                            System.out.print(getIntToString(DataArray[index]));
+                    } else {
+                        System.out.print(getIntToString(Data.get(inputInINT)));
 
-                        } else {
-                            System.out.print(getIntToString(DataArray[inputInINT]));
-                            IDArray[index] = inputInINT;
+                        input = System.in.read();
+                        Data.add(input);
 
-                            input = System.in.read();
-                            DataArray[index] = input;
-                            System.out.print(getIntToString(DataArray[index]));
+                        System.out.print(getIntToString(Data.get(index)));
 
-                        }
-                        index++;
                     }
+                    index++;
+                }
+
             }
             System.out.println("");
             System.out.println("Printing Arrays");
-            dumpArrays(IDArray,true);
-            dumpArrays(DataArray,false);
 
         }
         catch(Exception e)
@@ -70,21 +66,9 @@ class decoder
 
     private static void Reset()
     {
-        IDArray = new int[20];
-        DataArray = new int[20];
+        Data.clear();
+        Data.add(0);
         index = 1;
-    }
-    private static void dumpArrays(int [] array,boolean ID) throws UnsupportedEncodingException {
-        for(int x =1; x <array.length; x++ )
-        {
-            if(ID) {
-                System.out.print(array[x]);
-            }
-            else {
-                System.out.print(getIntToString(array[x]));
-            }
-        }
-        System.out.println("");
     }
     private static int getIntFromAxcii(int n)
     {
