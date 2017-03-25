523fae00e0ff22467341c4c07529039113ae7810
Joseph Boyd <josephboyd9@gmail.com>
2017-03-25 14:32:58 +1300

    splits phrase number from data

--- Bitpacker.java
+++ Bitpacker.java
@@ -1,32 +1,46 @@
-import java.util.LinkedList<E>;
+import java.util.*;
+import java.lang.*;
+import java.nio.ByteBuffer;
 
 class Bitpacker{
-    private int nextOut=0;
+    private static int nextOut=0, nextBit=0, maxphrase=0;
     public static void main(String[] args){
-        List<byte> buf = new LinkedList<byte>();
-	byte c;
-	while((c=System.in.read())!=-1){
-	    if(buf.getLast()==10){
-		buf.removeLast();
-		if(c==10){
+        LinkedList<Byte> buf = new LinkedList<Byte>();
+	byte c, temp;
+	try{
+	    buf.addLast((byte)System.in.read());
+	    while((c=(byte)System.in.read())>-1){
+		if(buf.getLast()==10){
+		    buf.removeLast();
+		    if(c==10){
+			buf.addLast(c);
+			pack(buf);
+			buf.clear();
+		    }else{
+			pack(buf);
+			buf.clear();
+			buf.addLast(c);
+		    }
+		}else
 		    buf.addLast(c);
-		    pack(buf);
-		    buf.clear();
-		}else{
-		    pack(buf);
-		    buf.clear();
-		    buf.addLast(c);
-		}
-	    }else
-		buf.addLast(c);
+	    }
+	    if(buf.size()>0)
+		packNum(buf);
+	}catch(Exception e){
+	    e.printStackTrace();
 	}
-	if(buf.size()>0)
-	    packNum(buf);
     }
-    private static void pack(List<byte> buf){
-
+    private static void pack(LinkedList<Byte> buf){
+	byte data=buf.removeLast();
+	int num=0;
+	for(Byte b:buf){
+	    num*=10;
+	    num+=Integer.parseInt(new String(new byte[] {b}));
+	}
+	
+	
     }
-    private static void packNum(List<byte> buf){
+    private static void packNum(List<Byte> buf){
 
     }
 }
