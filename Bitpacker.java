1068deedd0a3063df14c2399ad3e411bbfbf927f
Joseph Boyd <josephboyd9@gmail.com>
2017-03-26 13:33:54 +1300

    first word is packing correctly. After that, not so much.

--- Bitpacker.java
+++ Bitpacker.java
@@ -3,13 +3,14 @@ import java.lang.*;
 import java.nio.ByteBuffer;
 
 class Bitpacker{
-    private static int nextOut=0, nextBit=0, phraseLength=1;
+    private static int nextOut=0, nextBit=0, phraseLength=1, lineNum=1;
     public static void main(String[] args){
         LinkedList<Byte> buf = new LinkedList<Byte>();
 	byte c, temp;
 	try{
 	    buf.addLast((byte)System.in.read());
 	    while((c=(byte)System.in.read())>-1){
+		//System.out.println("input: "+Integer.toBinaryString(c));
 		if(buf.getLast()==10){
 		    buf.removeLast();
 		    if(c==10){
@@ -37,16 +38,15 @@ class Bitpacker{
 	if(buf.size()!=0){
 	    byte data=buf.removeLast();
 	    int num=0;
-	    for(Byte b:buf){
+	    for(byte b:buf){
 		num*=10;
 		num+=Integer.parseInt(new String(new byte[] {b}));
 	    }
-	    //right now, this is only checking for current number. should be using highest possible number.
-	    if(dropZeros(Integer.toBinaryString(num)).length()>phraseLength)
-		phraseLength++;
 	    pack(num, phraseLength);
-	    //7 bits in a java byte?? maybe they should just be stored in ints.
-	    pack(data, 7);
+	    pack(data, 8);
+	    lineNum++;
+	    if(dropZeros(Integer.toBinaryString(lineNum)).length()>phraseLength)
+		phraseLength++;
 	}
     }
     private static void pack(int input, int bits){
@@ -55,11 +55,15 @@ class Bitpacker{
 	if(shift<0)
 	    leftovers=(2^(-shift))&input;
 	input<<=shift;
+	/*System.out.println("bits: "+Integer.toString(bits));
+	System.out.println("nextBit: "+Integer.toString(nextBit));
+	System.out.println("shift: "+Integer.toString(shift));
+	System.out.println(Integer.toBinaryString(input));*/
 	nextOut|=input;
 	nextBit+=bits;
 	if(nextBit>=32){
 	    //print nextOut as binary
-	    System.out.print(Integer.toBinaryString(nextOut));
+	    System.out.println(Integer.toBinaryString(nextOut));
 	    nextOut=leftovers<<32+shift;
 	    nextBit=-shift;
 	}
